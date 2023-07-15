405668f8ed5d6b8a87ccfd700c486a3d83926e63
down1l <merkulov@downil.ru>
2023-07-15 16:43:06 +0300

    make libs file

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import requests
+from bs4 import BeautifulSoup as soup
+
 from time import sleep
 
-while True:
-    sleep(3)
-    status_code = requests.get("https://www.zveromir.ru/shop").status_code
-    print(status_code)
-    
+
+URL = "https://www.zveromir.ru/"
+
+def getLinksFromMainPage(url = URL) -> list:
+    html = requests.get(URL).text
+    parser = soup(html, "html.parser")
+    main_links = parser.find_all("a", class_ ="do-popdown")
+    for link in range(len(main_links)):
+        main_links[link] = main_links[link]["href"]
+    return main_links
+
+main_links = getLinksFromMainPage()
+print(main_links)
